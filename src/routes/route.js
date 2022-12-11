f80eaebebb1efc0d56d4b55a05d6f9b9cb80d87f
Chetan-Singh-Chouhan <mr.chiragchouhan@gmail.com>
2022-12-11 23:30:12 +0530

    updates

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,11 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const intro = require('./introduction')
-const employee = require('./employee')
-const _ = require('underscore')
-const mentorModule = require('../abc/xyz/myModule'); 
 const req = require('express/lib/request');
-const { route } = require('express/lib/application');
 
 moviesArray=['Rang de basanti', 'The shining', 'Lord of the rings', 'Batman begins']
 moviesObject=[ 
@@ -26,80 +21,17 @@ moviesObject=[
      "name": "Finding Nemo"
     }]
 router.get("/movies", function(req, res){
-    
     res.send(moviesArray)
 })
-
 router.get("/movies/:indexNumber", function(req, res){
     (req.params.indexNumber<moviesArray.length)?res.send(moviesArray[req.params.indexNumber]):res.send('Please use valid Index')
 })
 router.get("/films", function(req, res){
-    
        res.send(moviesObject)
 })
 router.get("/films/:filmId",function(req,res){
-    (req.params.filmId<moviesObject.length)?res.send(moviesObject[req.params.filmId]):res.send('Please use valid Index')
-})
-
-router.get('/test-me', function (req, res) {
-    console.log("email from introduction module", intro.myEmail)
-    intro.myFunction('Sabiha')
-    console.log("email from employee module", employee.myEmail)
-
-    const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let result = _.first(days, 4)
-    console.log(`Result from underscore function is ${result}`)
-    console.log(`The mentor of the day is ${mentorModule.mentor}`)
-
-    res.send('any dummy text from route handler 1')
-});
-
-
-router.get('/test-me', function(req, res){
-    console.log("I am here")
-    res.send("any dummy text from route handler 2")
-})
-
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
-
-// PATH Param example
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use many ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
+    (req.params.filmId < moviesObject.length && req.params.filmId > 0 )?res.send(moviesObject[req.params.filmId-1]):res.send("No movie exists with this id")
     
-    res.send('Dummy response')
-})
-
-// PATH Param example
-router.get("/profile/:name", function(req, res){
-    console.log('Printing the request to find out wjere name is stored',req.params)
-    console.log('user name is',req.params.name)
-    //console.log(`User requesting for profile is ${name}`)
-    res.send("dummy details")
-})
-
-// Query Param example
-router.get("/shoes", function(req, res){
-    console.log("The filter options for shoes are -",req.query)
-    //req.query.size
-    //req.query.brand
-    res.send("dummy shoes response")
 })
 
 module.exports = router;
