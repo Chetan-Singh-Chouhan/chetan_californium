c5e0172ce95f135446ea5271a45a0131c8fb0814
Chetan-Singh-Chouhan <mr.chiragchouhan@gmail.com>
2022-12-23 14:21:25 +0530

    middleware_ assignment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
+const address = require('address')
+const moment = require('moment');
 const app = express();
-const { default : mongoose } = require('mongoose')
 
+ 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect("mongodb+srv://Chetan-functionUp:9EFNNLl4q6IIvejN@cluster0.evsfgyx.mongodb.net/W5D3_populate_reference_assignment_db?retryWrites=true&w=majority",{useNewUrlParser:true})
-.then(()=>console.log("Mongo db is connected"))
-.catch(err=>console.log(err))
+
+app.use(
+    function(req,res,next){
+     let mycurrentIpAddress = address.ip()  //it will fetch my system ip address
+     let currentTimeDate = moment().format('YYYY-MM-DD, h:mm:ss')  //it format and store my current date and time
+     let myCurrentRoute = req.originalUrl
+     console.log(currentTimeDate,mycurrentIpAddress,myCurrentRoute)
+     next()
+})
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const bookController=require('../controllers/storeBookData')
 
-router.post("/createAuthor",bookController.storeAuthorData)
-router.post("/createPublisher",bookController.storePublisherData)
-router.post("/createBook",bookController.storeBookData)
-router.get("/getBook",bookController.getBookData)
-router.put("/book",bookController.updateBook)
+
+router.get("/api1",function(req,res){
+   res.send("This is First Api")
+})
+router.get("/api2",function(req,res){
+    res.send("This is Second Api")
+ })
+ router.get("/api3",function(req,res){
+    res.send("This is Third Api")
+ })
+
 module.exports = router;
